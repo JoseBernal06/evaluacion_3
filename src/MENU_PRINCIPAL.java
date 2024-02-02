ccc68f30bc47bcdcac56a0a46edac4ba5eaf5095
Jose Mateo Bernal <jose.bernal@epn.edu.ec>
2024-02-02 11:21:37 -0500

    Initial commit

--- src/MENU_PRINCIPAL.java
+++ src/MENU_PRINCIPAL.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class MENU_PRINCIPAL {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MENU_PRINCIPAL {
+    private JLabel texto_bienevenida;
+    private JPasswordField passwordField1;
+    private JLabel texto_password;
+    private JButton boton_validar;
+    JPanel panel_menu;
+
+    public MENU_PRINCIPAL() {
+        boton_validar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String clave_quemada="1234";
+
+            }
+        });
+    }
 }
