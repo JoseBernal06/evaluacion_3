c8363aef25b19628b5070e4c68d416efb84db97b
Jose Mateo Bernal <jose.bernal@epn.edu.ec>
2024-02-02 11:21:37 -0500

    segunda version

--- src/MENU_PRINCIPAL.java
+++ src/MENU_PRINCIPAL.java
@@ -1,10 +1,11 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MENU_PRINCIPAL {
     private JLabel texto_bienevenida;
-    private JPasswordField passwordField1;
+    private JPasswordField ingresado_password;
     private JLabel texto_password;
     private JButton boton_validar;
     JPanel panel_menu;
@@ -15,7 +16,23 @@ public class MENU_PRINCIPAL {
             public void actionPerformed(ActionEvent e) {
 
                 String clave_quemada="1234";
+                String usuario=ingresado_password.getText();
 
+                if (usuario.equals(clave_quemada)){
+                    //System.out.println("hola");
+                    JFrame frame_2 = new JFrame("MENU OPCIONES");
+                    //frame_2.setContentPane(new MENU_OPCIONES().panel_opciones);
+                    frame_2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    frame_2.setSize(300,300);
+                    frame_2.pack();
+                    frame_2.setVisible(true);
+                    Main.frame.dispose();
+
+                }
+                else{
+                    System.out.println("adios");
+                    JOptionPane.showMessageDialog(null, "CLAVE INCORRECTA");
+                }
             }
         });
     }
