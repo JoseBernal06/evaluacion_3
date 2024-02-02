ba75e7e928ac6a5c4d0b920890888429543cef30
ConKerazoE2906 <pablo.erazo01@epn.edu.ec>
2024-02-02 12:13:45 -0500

    Boton 3

---
+++ src/RETIRO.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class RETIRO {
+    private JButton a4Button;
+    private JButton a5Button;
+    private JButton a8Button;
+    private JButton a7Button;
+    private JButton a1Button;
+    private JButton a2Button;
+    private JButton a3Button;
+    private JButton a6Button;
+    private JButton a0Button;
+    private JButton button11;
+    private JButton a9Button;
+    private JTextField textField1;
+}
